dfe8e76e6c8bbfea25091d957c8577b903fbe72b
dupski <russ@paraflyer.net>
2014-12-30 20:57:06 +0000

    Added a self._app property for http controllers which references the running RevApp. Also renamed render_template back to _render_template so it doesn't get set up as an endpoint.

--- rev/http/controllers.py
+++ rev/http/controllers.py
@@ -1,11 +1,13 @@
 
 from flask.ext.classy import FlaskView, route
 from flask import render_template as flask_render_template
+from flask import current_app
 
 class RevHTTPController(FlaskView):
+    
+    @property
+    def _app(self):
+        return current_app
         
-    def render_template(self, template_name, **kwargs):
-        test = {
-            'name' : 'Rev Framework Application',
-        }
+    def _render_template(self, template_name, **kwargs):
         return flask_render_template(template_name, **kwargs)
