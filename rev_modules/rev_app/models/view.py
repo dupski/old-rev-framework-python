2ccb5123048094c52d1b4526e14f9f4c4dbed3e4
dupski <russ@paraflyer.net>
2014-12-31 00:14:05 +0000

    Renamed rev_app model modules to conform with PEP8

---
+++ rev_modules/rev_app/models/view.py
@@ -0,0 +1,31 @@
+
+from rev.db import models, fields
+from rev.i18n import translate as _
+
+ACTION_TYPES = [
+    ('menu', _('Menu')),
+    ('model_view', _('Model View')),
+]
+
+MODEL_VIEW_TYPES = [
+    ('list', _('List View')),
+    ('form', _('Form View')),
+]
+
+class RevView(models.RevModel):
+
+    _description = 'Rev App UI Action'
+    
+    name = fields.TextField(_('Action Name'))
+    type = fields.SelectionField(_('Action Type'), ACTION_TYPES)
+    
+    # Fields for 'model_view' actions:
+    model = fields.TextField(_('Model Name'), required=False)
+    view_type = fields.MultiSelectionField(_('View Type List'), MODEL_VIEW_TYPES, required=False)
+    views = fields.RecordLinkField(_('Menu'), 'RevView', multi=True, required=False)
+    filter = fields.JSONField(_('Record Filter'), required=False)
+    
+    # Fields for 'menu' actions:
+    menu = fields.RecordLinkField(_('Menu'), 'RevMenu', required=False)
+
+    _unique = ['name']
