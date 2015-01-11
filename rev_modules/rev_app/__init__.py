ef99e02e32b66b3aa46c705b357d38912e375140
dupski <russ@paraflyer.net>
2015-01-11 13:52:06 +0000

    Load JS and CSS based on module configuration files

--- rev_modules/rev_app/__init__.py
+++ rev_modules/rev_app/__init__.py
@@ -3,7 +3,20 @@ from .jinja_funcs import rev_view
 
 def after_app_load(app, prev_mod_info, syncdb):
 
-    # add rev_view() function to jinja2 context
+    javascript = []
+    css = []
+    for mod_name in app.module_load_order:
+        if 'javascript' in app.module_info[mod_name]:
+            javascript.extend(app.module_info[mod_name]['javascript'])
+        if 'css' in app.module_info[mod_name]:
+            css.extend(app.module_info[mod_name]['css'])
+
+    
     @app.context_processor
-    def setup_rev_view_jinja_function():
-        return {'rev_view' : rev_view}
+    def setup_jinja_context():
+        return {
+            # add rev_view() function to jinja2 context
+            'rev_view' : rev_view,
+            'js_files' : javascript,
+            'css_files' : css,
+        }
