2b2f9734543d95ed8602b87e80fc577ae16d3c62
dupski <russ@paraflyer.net>
2015-01-21 21:03:28 +0000

    Core modules moved to rev-core-modules branch

--- rev_modules/rev_app/models/__init__.py
+++
@@ -1,9 +0,0 @@
-
-from rev.db import Model, fields
-from rev.db.mixins import XMLDataMixin
-from rev.i18n import translate as _
-
-class MetadataModel(XMLDataMixin, Model):
-    """
-    Base class for rev_app metadata models
-    """
